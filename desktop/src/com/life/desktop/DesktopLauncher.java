7c280d429e23632e5f5428a6e4f81ba90da0a500
Crumster <crumster88@gmail.com>
2016-02-01 18:40:58 +0200

    Add initial code

---
+++ desktop/src/com/life/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.life.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.life.JustAnotherLife;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new JustAnotherLife(), config);
+	}
+}
