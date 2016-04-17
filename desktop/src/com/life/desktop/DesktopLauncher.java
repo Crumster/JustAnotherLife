fb51a8a4a171560eee0192217a9a5b1612ef563b
Crumster <crumster88@gmail.com>
2016-04-17 19:47:47 +0300

    Enable vsync

--- desktop/src/com/life/desktop/DesktopLauncher.java
+++ desktop/src/com/life/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Just Another Life";
 		//config.addIcon("icon/life.png", Files.FileType.Internal);
+		config.vSyncEnabled = true;
 		new LwjglApplication(new JustAnotherLife(), config);
 	}
 }
